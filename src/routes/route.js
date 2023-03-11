c7b919dd408501cec55aaa9261fd7caba0ea4403
SonuKumarYadav9 <skumaryadav.man@gmail.com>
2023-03-11 14:14:11 +0530

    File change

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const userController = require("../../controllers/userController");
-const productController = require("../../controllers/productController");
+const userController = require("../controllers/userController");
+const productController = require("../controllers/productController");
 
 const { registerUser } = userController;
 const { createProduct} = productController;
